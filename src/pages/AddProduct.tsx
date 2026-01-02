d86e8b4fd5d35245915951366aa8a4d309fe899d
Tanmay <patiltanmayanant121@gmail.com>
2026-01-02 12:52:10 +0530

    Added meatrial ui textfield

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,11 +1,77 @@
-import React from "react";
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
 
-const AddProduct = () => {
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  image: string;
+}
+
+export const AddProduct = () => {
+  const [product, setProduct] = useState<Omit<Product, "id">>({
+    name: "",
+    price: 0,
+    category: "",
+    image: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setProduct((prev) => ({
+      ...prev,
+      [name]: name === "price" ? Number(value) : value,
+    }));
+  };
+
   return (
     <div>
       <h1>Add Product</h1>
+
+      <form>
+        <div>
+          <TextField
+            label="Product Name"
+            name="name"
+            value={product.name}
+            onChange={handleChange}
+            fullWidth
+          />
+        </div>
+
+        <div style={{ marginTop: 12 }}>
+          <TextField
+            label="Price"
+            name="price"
+            type="number"
+            value={product.price}
+            onChange={handleChange}
+            fullWidth
+          />
+        </div>
+
+        <div style={{ marginTop: 12 }}>
+          <TextField
+            label="Category"
+            name="category"
+            value={product.category}
+            onChange={handleChange}
+            fullWidth
+          />
+        </div>
+
+        <div style={{ marginTop: 12 }}>
+          <TextField
+            label="Image URL"
+            name="image"
+            value={product.image}
+            onChange={handleChange}
+            fullWidth
+          />
+        </div>
+      </form>
     </div>
   );
 };
-
-export default AddProduct;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import Layout from "../Layout";
 import Home from "../pages/Home";
-import AddProduct from "../pages/AddProduct";
 import EditProduct from "../pages/EditProduct";
+import { AddProduct } from "../pages/AddProduct";
 
 const router = createBrowserRouter([
   {
